571c84c3d8e6228543b912ed50e95addff919b56
Aparna Dalal <34259886+aparnada7@users.noreply.github.com>
2018-05-05 17:13:39 -0700

    Update app.py
    skeleton for routing

--- Insurance- SmartContractApplication/routing/app.py
+++ Insurance- SmartContractApplication/routing/app.py
@@ -3,6 +3,31 @@ import jsonify
 
 app = Flask(__name__)
 
-@app.route("/login")
+
+@app.route("/")
+def loadIndex():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=['POST'])
 def login():
-    
+    return render_template("login.html")
+
+
+@app.route("/register", methods=['POST'])
+def registerUser():
+    return render_template("register.html")
+
+
+@app.route("/userbuy", methods=['POST'])
+def sell():
+    return render_template("userbuy.html")
+
+
+@app.route("/userclaim", methods=['POST'])
+def buy():
+    return render_template("userclaim.html")
+
+
+if __name__ == '__main__':
+    app.run(port=3002, debug=True)
