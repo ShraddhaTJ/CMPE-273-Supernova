5edf0788e837f4991be364a8348519914acf8fb0
Anudeepa2204 <deepashenoy4@gmail.com>
2018-05-07 14:41:49 -0700

    changes in contract

--- Insurance- SmartContractApplication/contracts/Insurance.sol
+++ Insurance- SmartContractApplication/contracts/Insurance.sol
@@ -16,7 +16,9 @@ contract Insurance{
         string benefactor_email;
         address user;
         policy_status flag;
+        policy_type type;
     }
+
 
     struct Pay{
         uint payment_id;
@@ -30,11 +32,13 @@ contract Insurance{
         uint amount;
     }
 
-    enum policy_status {ACTIVE, EXPIRED, CLAIMED}
+    enum policy_type {SILVER, GOLD, PLATINUM}
+    enum policy_status {ACTIVE, CLAIMED}
     
     uint policy_no = 0;
     mapping(address => User) public users_list;
     mapping(address => Policy) public user_policies;
+    mapping(address => unit) public policies_bought;
 
     
     function register(string fname, string lname, string email, string password) public {
@@ -43,25 +47,28 @@ contract Insurance{
         }
         users_list[msg.sender] = User(email, password, msg.sender, fname, lname, 1);
     }
+
     function get_user() public constant returns (string, string, string, uint){
         return (users_list[msg.sender].fname, users_list[msg.sender].lname, users_list[msg.sender].email, users_list[msg.sender].active); 
     }
 
-    function getUserByAddress(address requestAddress) public constant returns(string,string,string) {
-    return (users_list[requestAddress].fname, users_list[requestAddress].lname, users_list[requestAddress].email);  
+    function get_user_by_address(address requestAddress) public constant returns(string,string,string, uint, uint) {
+    return (users_list[requestAddress].fname, users_list[requestAddress].lname, users_list[requestAddress].email, policies_bought[requestAddress]);  
   }
 
     function take_insurance(string b_email) public 
     {
         user_policies[msg.sender] = Policy(policy_no, b_email, msg.sender, policy_status.ACTIVE);
+        policies_bought[msg.sender] = policies_bought[msg.sender] + 1;
     }
 
     function get_policy_status() public constant returns (policy_status){
         return user_policies[msg.sender].flag;
     }
-    function get_policy_details() public constant returns (uint, string, address, policy_status){
+
+    function get_policy_details() public constant returns (uint, string, address, policy_status, policy_type){
         var p = user_policies[msg.sender];
-        return (p.policy_number, p.benefactor_email, msg.sender, p.flag);
+        return (p.policy_number, p.benefactor_email, msg.sender, p.flag, p.type);
     }
 
     function claim_money() public {
---
+++ Insurance- SmartContractApplication/src/js/events.js
@@ -0,0 +1,26 @@
+function getUserDetails() {
+    get_user_by_address(web3.eth.accounts[0]);
+}
+
+
+function showHideBuySilver() {
+    if (document.getElementById('buy-silver')) {
+        document.getElementById('buy-gold').disabled = true;
+        document.getElementById('buy-platinum').disabled = true;
+    }
+}
+function showHideBuyGold() {
+    if (document.getElementById('buy-gold')) {
+        document.getElementById('buy-silver').disabled = true;
+        document.getElementById('buy-platinum').disabled = true;
+    }
+}
+function showHideBuyPlatinum() {
+    if (document.getElementById('buy-platinum')) {
+        document.getElementById('buy-silver').disabled = true;
+        document.getElementById('buy-gold').disabled = true;
+    }
+}
+function goToBuyPage() {
+    window.location = 'index.html';
+}
